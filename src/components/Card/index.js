6cdec7efa987067548fedb5a21a619c0c2a41c00
Jesse Manninen <jesse.manninen@gmail.com>
2023-04-04 19:18:46 +0300

    flex flex flex flex justify flex yes more flex

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,6 +1,6 @@
 import './styles/index.css';
 
-const Card = ({ animal, addLike, removeLike, removeAnimal, handleLikes }) => {
+const Card = ({ animal, removeAnimal, handleLikes }) => {
   return (
     <div className='card'>
       <div className='card-close-btn'>
@@ -12,25 +12,27 @@ const Card = ({ animal, addLike, removeLike, removeAnimal, handleLikes }) => {
         src={`https://source.unsplash.com/500x400/?${animal.name}`}
         alt={animal.name}
       />
-      <h2>{animal.name.charAt(0).toUpperCase() + animal.name.slice(1)}</h2>
-      <div className='card-likes'>
-        <button onClick={handleLikes} name={animal.name} value={-1}>
-          -
-        </button>
-        <div className='likes'>
-          <p className='like-count'>{animal.likes}</p>
-          {animal.likes >= 0 ? (
-            <i className='fa-solid fa-heart' style={{ color: '#ff0000' }} />
-          ) : (
-            <i
-              className='fa-solid fa-heart-crack'
-              style={{ color: '#ff0000' }}
-            />
-          )}
+      <div className='card-main'>
+        <h2>{animal.name.charAt(0).toUpperCase() + animal.name.slice(1)}</h2>
+        <div className='card-likes-area'>
+          <button onClick={handleLikes} name={animal.name} value={-1}>
+            -
+          </button>
+          <div className='likes'>
+            {animal.likes >= 0 ? (
+              <i className='fa-solid fa-heart' style={{ color: '#ff0000' }} />
+            ) : (
+              <i
+                className='fa-solid fa-heart-crack'
+                style={{ color: '#ff0000' }}
+              />
+            )}
+            <p className='like-count'>{animal.likes}</p>
+          </div>
+          <button onClick={handleLikes} name={animal.name} value={+1}>
+            +
+          </button>
         </div>
-        <button onClick={handleLikes} name={animal.name} value={+1}>
-          +
-        </button>
       </div>
     </div>
   );
